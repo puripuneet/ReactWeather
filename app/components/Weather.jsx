19c78adc0f6b00ff9981945cabbf4bb8fae90eb4
Puneet Puri <puneet@zikara.com>
2017-05-06 17:42:09 -0400

    Add error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
@@ -12,7 +13,9 @@ var Weather = React.createClass({
     handleSearch: function (location) {
         var that = this;
 
-        this.setState({isLoading: true});
+        this.setState({
+            isLoading: true,
+            errorMessage: undefined});
 
         openWeatherMap.getTemp(location).then(function (temp) {
             that.setState({
@@ -20,18 +23,18 @@ var Weather = React.createClass({
                 temp: temp,
                 isLoading: false
             });
-        }, function (errorMessage) {
+        }, function (e) {
             that.setState({
                 location: undefined,
                 temp: undefined,
-                isLoading: false
+                isLoading: false,
+                errorMessage: e.message
             });
-            alert(errorMessage);
         });
     },
     render: function () {
-        var {isLoading, temp, location} = this.state;
-
+        var {isLoading, temp, location, errorMessage} = this.state;
+ 
         function renderMessage () {
             if (isLoading) {
                 return <h3 className="text-center">Fetching weather...</h3>;
@@ -40,11 +43,20 @@ var Weather = React.createClass({
             }
         }
 
+        function renderError () {
+            if (errorMessage) {
+                return(
+                    <ErrorModal message={errorMessage}/>
+                );
+            }
+        }
+
         return (
             <div>
                 <h1 className="text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
+                {renderError()}
             </div>
         );
     }
